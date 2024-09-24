7e4f334282e63c4b1e04d45e84e21dc619cce657
f_ponce21 <ponce21francisco@gmail.com>
2024-09-24 16:24:36 -0700

    adding even or odd logic

--- src/main/java/com/ponce/week1/EvenOrOdd.java
+++ src/main/java/com/ponce/week1/EvenOrOdd.java
@@ -1,9 +1,20 @@
 package com.ponce.week1;
 
+import java.util.Scanner;
 
 public class EvenOrOdd {
 
 	public static void main(String[] args) {
-		//adding code
-}
+		// system.in is user input
+		Scanner input = new Scanner(System.in);
+		String name = input.next();
+
+		int result = Integer.parseInt(name);
+
+		if (result % 2 == 0) {
+			System.out.println(result + " is an even number.");
+		} else {
+			System.out.println(result + " is an odd number.");
+		}
+	}
 }
